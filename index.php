cea61eb31735b9f08888d8a0ca9af03492bc9f0b
helpful-articles <77534653+helpful-articles@users.noreply.github.com>
2021-01-20 02:14:58 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&utm_placement={{placement}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}}&num={num} 
 
 require_once dirname(__FILE__) . '/kclient.php';
-$client = new KClient('https://app-anntrack.ru/api.php?', 'rt4Tv8H9HMcRG7T4');
+$client = new KClient('https://app-anntrack.ru/api.php?', 'bL8TrL2wVVvRbLCP');
 $client->sendAllParams();       // to send all params from page query
 $client->forceRedirectOffer();       // redirect to offer if an offer is chosen
 // $client->param('sub_id_5', '123'); // you can send any params
